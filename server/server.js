fb2c06806efcec90a2649a79eda917cdd0800497
rogervalentim <valentim.roger33@gmail.com>
2023-02-28 19:26:25 -0300

    add books with database

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from "path"
 import cors from "cors";
 const app = express();
 
@@ -9,7 +10,15 @@ import bookRoutes from './src/routes/bookRoutes.js';
 
 
 app.use(express.json());
-app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+    app.use(express.static(path.join("public")));
+    app.use(cors());
+    next();
+});
+
 
 app.use(userRoutes);
 app.use(bookRoutes);
